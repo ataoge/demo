c67f7970e912a71dbe4c6da1974789eafb0ba1e4
loveht <loveht@aliyun.com>
2016-11-10 09:15:20 +0800

    Add Vue.js

--- src/WebApplication/wwwroot/js/site.js
+++ src/WebApplication/wwwroot/js/site.js
@@ -1,1 +1,25 @@
 ﻿// Write your Javascript code.
+Vue.component('todo-item', {
+  // The todo-item component now accepts a
+  // "prop", which is like a custom attribute.
+  // This prop is called todo.
+  props: ['todo'],
+  template: '<li>{{ todo.text }}</li>'
+});
+
+var app = new Vue({
+  el: '#app',
+  data: {
+    groceryList: [
+      { text: 'Vegetables' },
+      { text: 'Cheese' },
+      { text: 'Whatever else humans are supposed to eat' }
+    ],
+    message: 'Hello Vue.js!'
+  },
+  methods: {
+    reverseMessage: function () {
+      this.message = this.message.split('').reverse().join('')
+    }
+  }
+});
